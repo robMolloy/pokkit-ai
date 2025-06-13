12b251b4d831be48804f904ca64923f971e500cd
robmolloy <robmolloy@hotmail.co.uk>
2025-06-13 16:32:56 +0100

    improve ease of switching to/from remote/local

--- src/config/pocketbaseConfig.ts
+++ src/config/pocketbaseConfig.ts
@@ -1,5 +1,8 @@
 import PocketBase from "pocketbase";
 
-// export const pb = new PocketBase("http://127.0.0.1:8090");
+const pbInstanceMap = {
+  pbLocal: () => new PocketBase("http://127.0.0.1:8090"),
+  pbRemote: () => new PocketBase("https://romolo.pockethost.io"),
+};
 
-export const pb = new PocketBase("https://romolo.pockethost.io");
+export const pb = pbInstanceMap.pbLocal();
